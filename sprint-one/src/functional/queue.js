604d12cc2e2cdcdcfcdca611eb08b746f01be649
Joyce Liu <liujoycec@gmail.com>
2015-08-19 20:01:36 -0700

    Completed functional instantiation of Stack and Queue, but for some reason not passing stack's spec for not using the keyword 'this'.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function(){
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,24 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    someInstance[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function(){
+    if (count) {
+      count--;
+    }
+    var next = someInstance[0];
+    for (var i = 0; i < count; i++) {
+      someInstance[i] = someInstance[i + 1];
+    }
+    delete someInstance[count];
+    return next;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,27 @@
 var Stack = function(){
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {}; // What is this for?
 
   // Implement the methods below
   someInstance.push = function(value){
+    someInstance[count] = value;
+    count++;
   };
 
   someInstance.pop = function(){
+    if (count) {
+      count--;
+    }
+    var popped = someInstance[count];
+    delete someInstance[count];
+    return popped;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
