54679b2febf7ecb1559b051b734bf077d7259822
Joyce Liu <liujoycec@gmail.com>
2015-08-20 15:29:39 -0700

    Completed stack.js and queue.js for pseudoclassical instantiation style, and passed all specs.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.count) {
+    this.count--;
+  }
+  var next = this[0];
+  for (var i = 0; i < this.count; i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[this.count];
+  return next;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+Stack.prototype.pop = function() {
+  if (this.count) {
+    this.count--;
+  }
+  var popped = this[this.count];
+  delete this[this.count];
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+};
