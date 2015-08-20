6789298eb3da410f81babc6c8be5fbf0e3e24d89
Joyce Liu <liujoycec@gmail.com>
2015-08-20 11:40:12 -0700

    Completed stack.js and queue.js for prototypal instantiation style, and passed all specs.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstanceOf = Object.create(queueMethods);
+  someInstanceOf.count = 0;
+  return someInstanceOf;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.count) {
+    this.count--;
+  }
+  var next = this[0];
+  for (var i = 0; i < this.count; i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[this.count];
+  return next;
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
 
 
