2353045ccffa0de2144475e3528bb2fcb838589d
Joyce Liu <liujoycec@gmail.com>
2015-08-20 16:34:36 -0700

    Completed tree.js and passed specs. Also answered time complexity question.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,26 +3,34 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   addChild: constant, because we are only adding a node to the end of an array and are not looping.
+   contains: linear, because the nodes only get operated on once
  */
