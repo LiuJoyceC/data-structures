bde0fea2e61c1f4f30977358282fc1c741a8d572
Joyce Liu <liujoycec@gmail.com>
2015-08-20 15:52:09 -0700

    Completed linkedList.js and passed specs.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var head = list.head;
+    if (head) {
+      list.head = head.next;
+      if (list.tail === head) {
+        list.tail = null;
+      }
+      return head.value;
+    }    
   };
 
   list.contains = function(target){
+    for (var node = list.head; node; node = node.next) {
+      if (node.value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
