4ccc81239a7571a8d216216b3f3bc658f55cb80b
Joyce Liu <liujoycec@gmail.com>
2015-08-20 11:24:49 -0700

    Completed stack.js and queue.js for functional-shared instantiation style, and passed all specs.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstanceOf = {};
+  someInstanceOf.count = 0;
+  _.extend(someInstanceOf, queueMethods);
+  return someInstanceOf;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.count) {
+    this.count--;
+  }
+  var next = this[0];
+  for (var i = 0; i < this.count; i++) {
+    this[i] = this[i + 1];
+  }
+  delete this[this.count];
+  return next;
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstanceOf = {};
+  someInstanceOf.count = 0;
+  _.extend(someInstanceOf, stackMethods);
+  return someInstanceOf;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  if (this.count) {
+    this.count--;
+  }
+  var popped = this[this.count];
+  delete this[this.count];
+  return popped;
+};
+
+stackMethods.size = function () {
+  return this.count;
+};
