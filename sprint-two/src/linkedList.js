37e830811146c6d661c71e8222be787c2fc40590
Joyce Liu <liujoycec@gmail.com>
2015-08-20 16:06:22 -0700

    Answered question about time complexity at the bottom of linkedList.js.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -47,4 +47,17 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+  list.addToTail: Constant, because you only operate on the tail,
+      regardless if we have 3 nodes or 1000000 nodes.
+
+  list.removeHead: Constant, because you only operate on the head,
+      regardless of how many nodes are in it.
+
+  list.contains: Linear, because the function iterates through the
+      chain to look for the target. If you have 3 nodes, it would
+      on average need to iterate through 1.5 nodes, and if you have
+      3000 nodes, you would have to on average iterate through 1500
+      nodes to find the target. Each new node incrementally adds a
+      constant amount of time.
  */
