b8b1a3823a405ae7f20036c1c7a5fc3ea243db64
Joyce Liu <liujoycec@gmail.com>
2015-08-22 10:54:31 -0700

    Refactored set.js to use the hash feature of JS objects so all methods now run in constant time.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,39 +7,30 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  if (!this.contains(item)) {
-    this._storage[item] = true;
-  }
-
-  // Note: even though we can accomplish the same result
-  // without the if statement,
-  // it is not the way a set works, and it would be 'cheating'
-  // with the hash feature of Javascript objects, causing
-  // it to be constant time.
-  // therefore, we included the loop to search for the item
-  // first to make sure we don't 'override' it in a true set.
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
-  for (var str in this._storage) {
-    if (str === item) {
-      return true;
-    }
-  }
-  return false;
+  return item in this._storage;
 };
 
 setPrototype.remove = function(item){
-  for (var str in this._storage) {
-    if (str === item) {
-      delete this._storage[str];
-    }
-  }
+  delete this._storage[item];
 };
+
+  // Note: In our first version of these methods,
+  // we intentionally made all of them run in linear time
+  // because we thought it would be "cheating" to
+  // use the hash feature of javascript objects,
+  // and wanted to make our methods consistent with
+  // how we believe the set data structure works
+  // However, we later read in the README instructions
+  // for the hash table that we should come back
+  // to our set and refactor it to run in constant time
+  // by taking advantage of the hash feature
 
 /*
  * Complexity: What is the time complexity of the above functions?
-   Add: Linear, because we look through each item to see if it exists.
-   Contains: Linear, because we look through each item to see if it exists.
-   Remove: Linear, because we search through each item to see which one to delete.
+   Add, remove, and contains are all constant, because we are now using
+   the hash feature of JS objects which are constant time.
  */
