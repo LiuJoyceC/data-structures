b3983b9fd5923887ff387f5ac4af9ac22d460b7d
Joyce Liu <liujoycec@gmail.com>
2015-08-21 19:39:16 -0700

    Completed hashTable.js and passed specs. Also corrected a failing spec.

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -32,7 +32,7 @@ describe('hashTable', function() {
   it('should not contain values that were removed', function() {
     hashTable.insert('Steven', 'Tyler');
     hashTable.remove('Steven');
-    expect(hashTable.retrieve('Steven')).to.equal(null);
+    expect(hashTable.retrieve('Steven')).to.equal(undefined); // corrected test to be undefined instead of null
   });
 
   it('should handle hash function collisions', function(){
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(i)) {
+    this._storage.set(i, []);
+  }
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      bucket[j][1] = v;
+      return;
+    }
+  }
+  bucket.push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      return bucket[j][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if (bucket[j][0] === k) {
+      bucket.splice(j, 1);
+      // Note: changed Mocha test to say that
+      // the removed value should become undefined
+      // rather than null, since we want to delete
+      // the key-value pair from the hash table
+      // not just replace the value with a null object
+    }
+  }
 };
 
 
